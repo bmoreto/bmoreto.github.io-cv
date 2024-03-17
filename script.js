29a59ecf6f8522504043d409ec286db6459c5f94
bmoreto <beatriz_moreto@hotmail.com>
2024-03-17 15:58:34 -0300

    otimizacoes

--- script.js
+++
@@ -1,5 +0,0 @@
-function botaoEnd(event) {
-  const projetoMais = document.querySelector(".projetos-mais");
-  const projetoContainer = document.querySelector(".projetos");
-  projetoMais.prepend(projetoContainer);
-}
